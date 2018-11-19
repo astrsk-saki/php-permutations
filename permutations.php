61607d2ad6af50b9ca6f8acdb4a37f0e0f54adb7
ɐmɐʞıʞɐs <sakikawa@astrsk.jp>
2018-11-19 17:20:05 +0900

    Update permutations.php

--- permutations.php
+++ permutations.php
@@ -1,27 +1,23 @@
 <?php
-function permutations($list, $num, $case=[], $all_case=[])
+function permutations($list, $length, $case=[], $results=[])
 {
     $list_count = count($list);
-    if ($num>$list_count){                  // エラー
+    if ($length>$list_count){               // エラー
         return [];
     }
-    if ($num==0 || $list_count==0){         // 探索終了
-        $all_case[] = $case;
-        return $all_case;
+    if ($length===0 || $list_count===0){    // 探索終了
+        $results[] = $case;
+        return $results;
     }
-
     for ($i=0; $i<$list_count; $i++){
         $target = $list;                    // 初期状態に戻す
         array_push($case, $target[$i]);     // 値を追加
-
         unset($target[$i]);                 // リストから値を削除
         $target = array_values($target);    // 添字再設定
-
         // 次の桁を探索
-        $all_case = permutations($target, $num-1, $case, $all_case);
-
+        $results = permutations($target, $length-1, $case, $results);
         array_pop($case);                   // 値を削除
     }
     
-    return $all_case;
+    return $results;
 }
